7a646101a4b723f0c5fd9f3d312386523d6ebe33
Balaji798 <balajiy798@gmail.com>
2024-11-15 20:15:24 +0530

    new update

---
+++ dist/assets/CancelOrder-Ete0pDKZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D2g3KFVD.js";const t=()=>r.jsx("div",{children:"CancelOrder"});export{t as default};
---
+++ dist/assets/internationl-shipping-XdVU5HHC.js
@@ -0,0 +1,1 @@
+const p="/assets/internationl-shipping-DzT3w0kd.jpg";export{p};
